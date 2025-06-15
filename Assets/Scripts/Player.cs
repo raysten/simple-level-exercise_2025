a8e9f5b8b18a8c25cff04febeee55e8364d978a7
raysten <raysten87@gmail.com>
2025-06-15 14:04:19 +0200

    Fixing wall collisions wip.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,7 +16,7 @@ public class Player : MonoBehaviour
     private float _movementSpeed = 10f;
 
     private InputAction _moveInputAction;
-    private Vector2 _movement;
+    private Vector2 _movementInput;
     private Vector3 _collisionMovement;
 
     private void OnEnable()
@@ -31,37 +31,52 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        _movement = _moveInputAction.ReadValue<Vector2>();
+        _movementInput = _moveInputAction.ReadValue<Vector2>();
     }
 
     private void FixedUpdate()
     {
-        var forwardDelta = _movement.y;
-        var rightDelta = _movement.x;
+        var forwardDelta = _movementInput.y;
+        var rightDelta = _movementInput.x;
         
-        var movementDelta = transform.forward * forwardDelta + transform.right * rightDelta;
+        var movementDelta = (transform.forward * forwardDelta + transform.right * rightDelta).normalized;
         
-        Debug.DrawRay(transform.position, movementDelta, Color.blue);
+        Debug.DrawRay(transform.position, movementDelta, Color.blue, 1f);
         
-        if (_movement.sqrMagnitude > 0f)
+        if (_movementInput.sqrMagnitude > 0f)
         {
-            Debug.DrawRay(transform.position, _collisionMovement, Color.red);
+            // Debug.LogError($"moveDelta: {movementDelta.magnitude}, collisionDelta: {_collisionMovement.normalized.magnitude}");
+            Debug.DrawRay(transform.position, _collisionMovement.normalized, Color.red, 1f);
             movementDelta += _collisionMovement.normalized;
         }
         
-        Debug.DrawRay(transform.position, movementDelta, Color.green);
+        Debug.DrawRay(transform.position, movementDelta, Color.black, 1f);
 
         _rigidbody.MovePosition(_rigidbody.position + movementDelta * (_movementSpeed * Time.fixedDeltaTime));
         
-        _collisionMovement = Vector2.zero;
+        // _collisionMovement = Vector3.zero;
     }
 
-    private void OnCollisionStay(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
+        _collisionMovement = Vector3.zero;
+        
+        Debug.LogError($"contacts count: {collision.contacts.Length}");
         foreach (var contactPoint in collision.contacts)
         {
             _collisionMovement += new Vector3(transform.position.x, 0f, transform.position.z) - new Vector3(contactPoint.point.x, 0f, contactPoint.point.z);
+            
+            // if normal >= 0.9f
+            // _collisionMovement += new Vector3(contactPoint.normal.x, 0f, contactPoint.normal.z);
         }
+
+        // var contactPoint = collision.contacts[0];
+        // _collisionMovement = new Vector3(transform.position.x, 0f, transform.position.z) - new Vector3(contactPoint.point.x, 0f, contactPoint.point.z);
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        _collisionMovement = Vector3.zero;
     }
 
     private void OnDisable()
