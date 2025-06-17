5ffa4cdcae69948548b02fe0c12c530e4e3c0e35
raysten <raysten87@gmail.com>
2025-06-17 18:16:54 +0200

    Working on moving platform.

---
+++ Assets/Scripts/Other/MovingPlatform.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Other
+{
+    public class MovingPlatform : MonoBehaviour
+    {
+        [SerializeField]
+        private Vector3 _startPosition;
+
+        [SerializeField]
+        private Vector3 _endPosition;
+
+        [SerializeField]
+        private float _speed;
+
+        private Vector3 _currentDestination;
+
+        private void Awake()
+        {
+            _currentDestination = _startPosition;
+        }
+
+        private void Update()
+        {
+            var direction = _currentDestination - transform.position;
+            transform.position += direction.normalized * (_speed * Time.deltaTime);
+        }
+    }
+}
